ae0f6351080cbc5a597a8c5ecf8f5def2d3181ec
JoshKD <54752293+JoshKD@users.noreply.github.com>
2024-12-13 00:15:21 -0500

    Update LabeledGraph.h

--- LabeledGraph.h
+++ LabeledGraph.h
@@ -6,14 +6,6 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#ifndef LABELED_GRAPH_H
-#define LABELED_GRAPH_H
-
-#include <set>
-#include <map>
-#include <vector>
-#include <string>
-#include <iostream>
 
 class LabeledGraph {
 public:
@@ -47,42 +39,6 @@ private:
     std::vector<std::set<int>> vertices;
     std::vector<std::string> labels;
     std::map<std::string, int> indexedLabels;
-};
-
-#endif
-
-
-class LabeledGraph {
-public:
-
-    // CONSTRUCTOR
-    LabeledGraph()  {nedges = 0; }
-    LabeledGraph(std::string a);
-
-    // MODIFICATION MEMBER FUNCTIONS
-    //adds a vertex with a specifc label
-    void add_vertex(std::string vlabel);
-    void display_related_vertices(std::string a);
-
-    //adds an edge from vertex with source label to vertex with target label
-    void add_edge(std::string source, std::string target);
-
-    // CONSTANT MEMBER FUNCTIONS
-    int V( ) const { return vertices.size(); } //number of vertices
-    int E( ) const { return nedges; }       //number of edges
-    bool contains(std::string vlabel) const;     //does graph contain vertex with label vlabel?
-    std::set<int> neighbors(int v) const;  //returns the vertices adjacent to vertex v
-    std::string label(int v) const;     //returns the label of vertex v
-    int index(std::string vlabel);           //returns the index of the vertex with label vlabel
-
-    // OVERLOADED OUTPUT OPERATOR
-    friend std::ostream& operator<< (std::ostream &out, const LabeledGraph &g);
-
-private:
-  int nedges;
-  std::vector<std::set<int> > vertices;
-  std::vector<std::string> labels;
-  std::map<std::string, int> indexedLabels;
 };
 
 #endif
